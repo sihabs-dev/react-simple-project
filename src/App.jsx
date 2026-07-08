421e21d02f482dc230f620d3280a55fbe52e33a2
sihabs-dev <mdsarks123@gmail.com>
2026-07-08 11:10:29 +0600

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Practice from "./practice";
+
+function App() {
+  return (
+    <>
+      <Practice></Practice>
+    </>
+  );
+}
+
+export default App;
---
+++ src/practice.jsx
@@ -0,0 +1,10 @@
+
+const practice = () => {
+    return (
+        <div>
+            <h1>I am your practice component</h1>
+        </div>
+    );
+};
+
+export default practice;
